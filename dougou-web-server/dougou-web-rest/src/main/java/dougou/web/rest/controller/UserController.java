44d0049b7c47bf216be29e45336eeffe52656c87
Wu Zihan <1048099346@qq.com>
2020-12-18 18:29:37 +0800

    ababab

--- dougou-web-server/dougou-web-rest/src/main/java/dougou/web/rest/controller/UserController.java
+++ dougou-web-server/dougou-web-rest/src/main/java/dougou/web/rest/controller/UserController.java
@@ -1,8 +1,24 @@
 package dougou.web.rest.controller;
 
+import dougou.web.core.service.IUserService;
+import dougou.web.rest.dto.SimpleResponse;
+import dougou.web.rest.vo.UserLoginVo;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.annotation.Resource;
+
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
-
+    @Resource
+    IUserService userServiceImpl;
+    @PostMapping
+    public SimpleResponse<Boolean> login(@RequestBody @Validated UserLoginVo userLoginVo){
+        userServiceImpl.getById()
+        return new SimpleResponse<Boolean>(true);
+    }
 }
