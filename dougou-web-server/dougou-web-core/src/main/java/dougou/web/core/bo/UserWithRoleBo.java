46ccfea5ee9a8bcba226f7189df03ee455d396a4
WuZihan <1048099346@qq.com>
2020-12-21 00:35:48 +0800

    just save

--- dougou-web-server/dougou-web-core/src/main/java/dougou/web/core/bo/UserWithRoleBo.java
+++ dougou-web-server/dougou-web-core/src/main/java/dougou/web/core/bo/UserWithRoleBo.java
@@ -1,9 +1,11 @@
 package dougou.web.core.bo;
 
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.List;
 @Data
+@ToString
 public class UserWithRoleBo {
     private String username;
     private String password;
